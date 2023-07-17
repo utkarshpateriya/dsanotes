5a5228d9053851d5c5f938e2dddbc8b788deb3ba
upateriya <utkarshpateriya.78@gmail.com>
2023-07-17 15:56:27 +0530

    some notes added

--- timecomplexity/iterations.py
+++ timecomplexity/iterations.py
@@ -1,1 +1,26 @@
-# Initial Commit 1
+# Time complexity is nothing but rate of growth of time with respect to input size
+
+N = 100 #Can be any positive integer
+
+#All the functions defined here:-
+
+#O(N) Example
+
+def iterateN(n):
+    for i in range(n):
+        print(n)
+
+#Number of iterations
+def countIteration(n):
+    '''
+    This will print the numbers from 0 to 100 i.e 100-0+1 = 101 iterations
+    '''
+    for i in range(0,100):
+        print(i)
+
+def logIterations(n):
+    '''
+    In this case iterations will be log2(n)
+    '''
+    for i in range(n):
+        i = i/2
